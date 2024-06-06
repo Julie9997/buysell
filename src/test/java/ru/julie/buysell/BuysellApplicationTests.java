ffa151e8f4c18ed83adbe20af2b5e9569e2a8792
Julie9997 <yuliy9997@gmail.com>
2024-06-06 19:56:17 +0300

    Create project. Avito clone

---
+++ src/test/java/ru/julie/buysell/BuysellApplicationTests.java
@@ -0,0 +1,12 @@
+package ru.julie.buysell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuysellApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
