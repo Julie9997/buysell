9014652191e1e3c8c6ddb7c28c989b155c30196e
Julie9997 <yuliy9997@gmail.com>
2024-06-07 16:47:47 +0300

    связь товара и пользователей

--- src/main/java/ru/julie/buysell/controller/ProductController.java
+++ src/main/java/ru/julie/buysell/controller/ProductController.java
@@ -13,6 +13,7 @@ import ru.julie.buysell.services.ProductService;
 
 import javax.imageio.IIOException;
 import java.io.IOException;
+import java.security.Principal;
 import java.util.Base64;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -24,16 +25,17 @@ public class ProductController {
     private final ProductService productService;
 
     @GetMapping("/")
-    public String products(@RequestParam(name = "title", required = false) String title, Model model) {
+    public String products(@RequestParam(name = "title", required = false) String title, Principal principal, Model model) {
         model.addAttribute("products", productService.listProducts(title));
+        model.addAttribute("user", productService.getUserByPrincipal(principal));
         return "products";
     }
 
     @PostMapping("product/create")
     public String createProduct(@RequestParam("file1") MultipartFile file1,
         @RequestParam("file2") MultipartFile file2,
-        @RequestParam("file3") MultipartFile file3,Product product) throws IOException {
-        productService.saveProduct(product, file1, file2, file3);
+        @RequestParam("file3") MultipartFile file3,Product product, Principal principal) throws IOException {
+        productService.saveProduct(principal, product, file1, file2, file3);
         return "redirect:/";
     }
 
--- src/main/java/ru/julie/buysell/services/ProductService.java
+++ src/main/java/ru/julie/buysell/services/ProductService.java
@@ -6,9 +6,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import ru.julie.buysell.models.Image;
 import ru.julie.buysell.models.Product;
+import ru.julie.buysell.models.User;
 import ru.julie.buysell.repositories.ProductRepository;
+import ru.julie.buysell.repositories.UserRepository;
 
 import java.io.IOException;
+import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,13 +21,15 @@ import java.util.List;
 public class ProductService {
 
     private final ProductRepository productRepository;
+    private final UserRepository userRepository;
 
     public List<Product> listProducts(String title) {
         if (title != null) return productRepository.findByTitle(title);
         return productRepository.findAll();
     }
 
-    public void saveProduct(Product product, MultipartFile file1, MultipartFile file2, MultipartFile file3) throws IOException {
+    public void saveProduct(Principal principal, Product product, MultipartFile file1, MultipartFile file2, MultipartFile file3) throws IOException {
+        product.setUser(getUserByPrincipal(principal));
         Image image1;
         Image image2;
         Image image3;
@@ -41,10 +46,15 @@ public class ProductService {
             image3 = toImageEntity(file3);
             product.addImageToProduct(image3);
         }
-        log.info("Saving new Product. Title: {}; Author: {}", product.getTitle(), product.getAuthor()); // поскольку метод toString() переопределен, подставит строковое представление продукт
+        log.info("Saving new Product. Title: {}; Author email: {}", product.getTitle(), product.getUser().getEmail()); // поскольку метод toString() переопределен, подставит строковое представление продукт
         Product productFromDb = productRepository.save(product);
         productFromDb.setPreviewImageId(productFromDb.getImages().get(0).getId());
         productRepository.save(product);
+    }
+
+    public User getUserByPrincipal(Principal principal) {
+        if(principal == null) return new User();
+        return userRepository.findByEmail(principal.getName());
     }
 
     private Image toImageEntity(MultipartFile file) throws IOException {
