24bfb29da89816c011bf703557a418b008f5aff0
Julie9997 <yuliy9997@gmail.com>
2024-06-10 13:10:36 +0300

    admin panel

--- src/main/java/ru/julie/buysell/controller/UserController.java
+++ src/main/java/ru/julie/buysell/controller/UserController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import ru.julie.buysell.models.User;
 import ru.julie.buysell.services.UserService;
@@ -37,6 +38,13 @@ public class UserController {
     @GetMapping("/hello")
     public String securityUrl() {
         return "hello";
+    }
+
+    @GetMapping("/user/{user}")
+    public String userInfo(@PathVariable("user") User user, Model model) {
+        model.addAttribute("user", user);
+        model.addAttribute("products", user.getProducts());
+        return "user-info";
     }
 
 }
